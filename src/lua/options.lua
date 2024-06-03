183986da1e5c126c07f2e23687e380562f396dde
Cheng Jingxuan <hbxycjx@outlook.com>
2024-06-03 15:17:43 +0800

    Small Opt

--- src/lua/options.lua
+++ src/lua/options.lua
@@ -3,9 +3,10 @@ vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'
 
 vim.opt.tabstop = 8
-vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.showmatch = true
 
 vim.opt.number = true
 vim.opt.relativenumber = false
