07443ebac8251f962f91e545ba4aa9de35e4f66e
Cheng Jingxuan <hbxycjx@outlook.com>
2024-06-15 16:20:40 +0800

    Improved Tab Stop

--- src/lua/options.lua
+++ src/lua/options.lua
@@ -3,6 +3,7 @@ vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'
 
 vim.opt.tabstop = 8
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
